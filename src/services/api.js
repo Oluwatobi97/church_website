282b12bf1f8e02d104ee389316d9cc9470ac645d
Oluwatobi97 <victortobi2000@gamil.com>
2026-05-23 10:20:32 +0100

    update the route chuch app

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,5 @@
 const API_BASE_URL =
-  import.meta.env.VITE_API_URL || "http://localhost:5000/api";
+  import.meta.env.VITE_API_URL || "https://church-website-k5rp.onrender.com";
 let accessToken = localStorage.getItem("accessToken");
 
 export const setAuthToken = (token) => {
